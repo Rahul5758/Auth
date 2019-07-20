291b2417a069fc7d656419d4f19b519a576c7032
Rahul5758 <rahulthapliyal5758@gmail.com>
2019-07-20 17:02:59 +0530

    Add files via upload

---
+++ Auth/Controllers/AuthControllerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Auth.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost("token")]
+        public IActionResult Token()
+        {
+            //string tokenString = "test";
+            var header = Request.Headers["Authorization"];
+            if (header.ToString().StartsWith("Basic"))
+            {
+                var credValue = header.ToString().Substring("Basic ".Length).Trim();
+                var usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue)); //admin:pass
+                var usernameAndPass = usernameAndPassenc.Split(":");
+                //check in DB username and pass exist
+
+                if (usernameAndPass[0] == "Admin" && usernameAndPass[1] == "pass")
+                {
+                    var claims = new List<Claim>();
+                    //claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("72f988bf-86f1-41af-91ab-2d7cd011db47"));
+                    var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                    var token = new JwtSecurityToken(
+                         issuer: "entservice",
+                         audience: "entservice",
+                         expires: DateTime.Now.AddMinutes(240),
+                         claims: claims,
+                         signingCredentials: signInCred
+                        );
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(tokenString);
+                }
+            }
+            return BadRequest("wrong request");
+
+            // return View();
+        }
+        }
+}
